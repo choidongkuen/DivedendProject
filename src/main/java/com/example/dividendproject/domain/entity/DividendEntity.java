605f0165a792426f445a0adf795ca857148289b9
choidongkuen <danaver12@daum.net>
2023-03-10 19:38:19 +0900

    feat : DividendEntity 생성자 추가

--- src/main/java/com/example/dividendproject/domain/entity/DividendEntity.java
+++ src/main/java/com/example/dividendproject/domain/entity/DividendEntity.java
@@ -1,5 +1,6 @@
 package com.example.dividendproject.domain.entity;
 
+import com.example.dividendproject.dto.Dividend;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,4 +25,10 @@ public class DividendEntity {
     private LocalDateTime date;
 
     private String dividend;
+
+    public DividendEntity(Long companyId, Dividend dividend) {
+        this.companyId = companyId;
+        this.date = dividend.getDate();
+        this.dividend = dividend.getDividend();
+    }
 }

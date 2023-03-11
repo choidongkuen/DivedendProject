e00f6baf8e01444a17501e2e2968e6183ea490bb
choidongkuen <danaver12@daum.net>
2023-03-11 22:33:37 +0900

    refactor : Spring Data Jpa 에 따른 함수명 변경

--- src/main/java/com/example/dividendproject/domain/repository/CompanyRepository.java
+++ src/main/java/com/example/dividendproject/domain/repository/CompanyRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
 
-    boolean existByTicker(String ticker);
+    boolean existsByTicker(String ticker);
 }

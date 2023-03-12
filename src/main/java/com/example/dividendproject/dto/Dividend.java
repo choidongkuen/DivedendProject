8f94cb2d98e0d467d04e612bf18cf8312370215f
choidongkuen <danaver12@daum.net>
2023-03-12 19:43:52 +0900

    feat : Dividend.class fromEntity 메소드 구현

--- src/main/java/com/example/dividendproject/dto/Dividend.java
+++ src/main/java/com/example/dividendproject/dto/Dividend.java
@@ -1,5 +1,6 @@
 package com.example.dividendproject.dto;
 
+import com.example.dividendproject.domain.entity.DividendEntity;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -15,4 +16,11 @@ public class Dividend {
     private LocalDateTime date;
 
     private String dividend;
+
+    public static Dividend fromEntity(DividendEntity dividendEntity) {
+        return Dividend.builder()
+                .date(dividendEntity.getDate())
+                .dividend(dividendEntity.getDividend())
+                .build();
+    }
 }

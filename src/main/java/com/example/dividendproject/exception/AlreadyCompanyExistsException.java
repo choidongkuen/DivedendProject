6eba6444db048ace16cb9a42018d54e384978360
choidongkuen <danaver12@daum.net>
2023-03-12 19:53:42 +0900

    feat :AlreadyCompanyExistsException 클래스 구현

---
+++ src/main/java/com/example/dividendproject/exception/AlreadyCompanyExistsException.java
@@ -0,0 +1,8 @@
+package com.example.dividendproject.exception;
+
+public class AlreadyCompanyExistsException extends RuntimeException {
+
+    public AlreadyCompanyExistsException(String message) {
+        super(message);
+    }
+}

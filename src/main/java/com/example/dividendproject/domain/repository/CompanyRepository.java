677a8fa0be9bf08698e830048e2e41c4a946eab2
choidongkuen <danaver12@daum.net>
2023-03-12 14:48:47 +0900

    feat : List<CompanyEntity> findAll() 메소드 추가

--- src/main/java/com/example/dividendproject/domain/repository/CompanyRepository.java
+++ src/main/java/com/example/dividendproject/domain/repository/CompanyRepository.java
@@ -4,8 +4,12 @@ import com.example.dividendproject.domain.entity.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
 
     boolean existsByTicker(String ticker);
+
+    List<CompanyEntity> findAll();
 }

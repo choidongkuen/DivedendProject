2d377aabaf745f6c912b93e54d68ff2511a22211
choidongkuen <danaver12@daum.net>
2023-03-21 00:22:24 +0900

    feat : Redis 캐시 관련 config Bean 구현

--- src/main/java/com/example/dividendproject/config/CacheConfig.java
+++ src/main/java/com/example/dividendproject/config/CacheConfig.java
@@ -2,11 +2,17 @@ package com.example.dividendproject.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @Configuration
@@ -19,9 +25,22 @@ public class CacheConfig { // redis cache config bean
     @Value("${spring.redis.port}")
     private final Integer port;
 
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) { // redis 캐시 관련 config bean
+
+        RedisCacheConfiguration conf = RedisCacheConfiguration.defaultCacheConfig()
+                              .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                              .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(conf)
+                .build();
+    }
+
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() { // redis connection 을 맺을 수 있는 Connection Factory
+    public RedisConnectionFactory redisConnectionFactory() { // redis connection 을 맺을 수 있는 Connection Factory(StandAlone)
 
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
         config.setHostName(host);

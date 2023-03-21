16748ad831d94485acee4be7cd662bb8d6ed74e8
choidongkuen <danaver12@daum.net>
2023-03-21 19:27:05 +0900

    Feat : MemberEntity UserDetails 인터페이스 구현

--- src/main/java/com/example/dividendproject/domain/entity/MemberEntity.java
+++ src/main/java/com/example/dividendproject/domain/entity/MemberEntity.java
@@ -1,12 +1,16 @@
 package com.example.dividendproject.domain.entity;
 
+import com.example.dividendproject.domain.constant.Authority;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Slf4j
 @Getter
@@ -14,7 +18,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "MemberEntity")
-public class MemberEntity extends BaseEntity {
+public class MemberEntity extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +30,37 @@ public class MemberEntity extends BaseEntity {
 
     @Column(name = "password", nullable = false, length = 50)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }

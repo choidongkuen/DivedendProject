34f96fb6865c0a6a609884c9cbe920998c19dd1c
choidongkuen <danaver12@daum.net>
2023-03-21 20:19:56 +0900

    Feat : AlreadyMemberSignupException 예외 클래스 구현

---
+++ src/main/java/com/example/dividendproject/exception/AlreadyMemberSignupException.java
@@ -0,0 +1,7 @@
+package com.example.dividendproject.exception;
+
+public class AlreadyMemberSignupException extends RuntimeException {
+    public AlreadyMemberSignupException(String message) {
+        super(message);
+    }
+}

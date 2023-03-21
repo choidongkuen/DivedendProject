6c40e8391f27d2d611cc0aa4c6217d5d3c5a771e
choidongkuen <danaver12@daum.net>
2023-03-21 20:10:08 +0900

    Fix : MemberRepository 메소드 명명법에 대한 오류 수정
    Reference : https://velog.io/@choidongkuen/Springboot-Could-not-create-query-for-public-abstract-java.util.Optional-%EC%98%A4%EB%A5%98-%EC%88%98%EC%A0%95

--- src/main/java/com/example/dividendproject/domain/repository/MemberRepository.java
+++ src/main/java/com/example/dividendproject/domain/repository/MemberRepository.java
@@ -2,11 +2,13 @@ package com.example.dividendproject.domain.repository;
 
 import com.example.dividendproject.domain.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-    Optional<MemberEntity> findByUsername(String userName); // 회원 이름을 통해 회원 엔티티를 찾는 메소드
+    Optional<MemberEntity> findByUserName(String userName); // 회원 이름을 통해 회원 엔티티를 찾는 메소드
 
-    boolean existsByUsername(String userName); // 회원가입을 할때 이미 존재하는 회원인지 확인하기 위한 메소드
+    boolean existsByUserName(String userName); // 회원가입을 할때 이미 존재하는 회원인지 확인하기 위한 메소드
 }

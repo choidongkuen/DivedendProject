3884f94ce25ecce31aa6b66899ef839fac8cc70f
choidongkuen <danaver12@daum.net>
2023-03-21 19:14:54 +0900

    fix : Authority 수정

--- src/main/java/com/example/dividendproject/domain/constant/Authority.java
+++ src/main/java/com/example/dividendproject/domain/constant/Authority.java
@@ -1,6 +1,15 @@
 package com.example.dividendproject.domain.constant;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
 public enum Authority {
-    ROLE_READ,
-    ROLE_WRITE
+
+    MEMBER("일반 회원"),
+    ADMIN("관리자");
+
+    private final String state;
 }

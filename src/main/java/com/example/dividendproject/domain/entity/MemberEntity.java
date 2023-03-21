d0967e17d8be62d6bf42734b7df704fdef2c5562
choidongkuen <danaver12@daum.net>
2023-03-21 19:13:30 +0900

    feat : MemberEntity 구현
    회원가입을 위한 회원 엔티티 클래스 구현

---
+++ src/main/java/com/example/dividendproject/domain/entity/MemberEntity.java
@@ -0,0 +1,29 @@
+package com.example.dividendproject.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Slf4j
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "MemberEntity")
+public class MemberEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String userName;
+
+    @Column(name = "password", nullable = false, length = 50)
+    private String password;
+}

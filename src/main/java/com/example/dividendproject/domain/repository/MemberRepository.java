808a0009b4985fee114e5d91d66a2835c8f63d60
choidongkuen <danaver12@daum.net>
2023-03-23 03:58:16 +0900

    Fix : MemberRepository 메소드 변경

--- src/main/java/com/example/dividendproject/domain/repository/MemberRepository.java
+++ src/main/java/com/example/dividendproject/domain/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-    Optional<MemberEntity> findByUserName(String userName); // 회원 이름을 통해 회원 엔티티를 찾는 메소드
+    Optional<MemberEntity> findByEmail(String email); // 회원 이메일을 통해 회원 엔티티를 찾는 메소드
 
-    boolean existsByUserName(String userName); // 회원가입을 할때 이미 존재하는 회원인지 확인하기 위한 메소드
+    boolean exitsByEmail(String email); // 회원가입을 할때 이미 존재하는 회원인지 확인하기 위한 메소드
 }

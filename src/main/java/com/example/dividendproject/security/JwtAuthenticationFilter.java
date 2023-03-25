d651a2d698d52b7cb6e07cd8b0727dc24d7f2735
choidongkuen <danaver12@daum.net>
2023-03-25 15:09:06 +0900

    Feat : JwtAuthenticationFilter doFilterInternal & resolveTokenFromToken 메소드 구현

--- src/main/java/com/example/dividendproject/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/dividendproject/security/JwtAuthenticationFilter.java
@@ -1,4 +1,52 @@
 package com.example.dividendproject.security;
 
-public class JwtAuthenticationFilter {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter { // 컨트롤러를 거치기 전 필터를 먼저 거침
+
+    public static final String TOKEN_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer";
+
+    private final TokenProvider tokenProvider;
+
+    // JWT : Authorization: "Bearer dasxcaewxacsdsadwxsad"
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String token = this.resolveTokenFromToken(request);
+
+        if(StringUtils.hasText(token) && this.tokenProvider.validateToken(token)) {
+
+            // 토큰 유효성 검증
+
+        }
+
+    }
+
+    private String resolveTokenFromToken(HttpServletRequest request) {
+
+        String token = request.getHeader(TOKEN_HEADER);
+
+        if (!ObjectUtils.isEmpty(token) && token.startsWith(TOKEN_PREFIX)) {
+            return token.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
+    }
 }

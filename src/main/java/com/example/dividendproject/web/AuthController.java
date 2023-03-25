76409b31e19710c98368e17a82ac434ba62e9398
choidongkuen <danaver12@daum.net>
2023-03-25 13:56:44 +0900

    Feat : 회원가입 컨트롤러 구현

--- src/main/java/com/example/dividendproject/web/AuthController.java
+++ src/main/java/com/example/dividendproject/web/AuthController.java
@@ -1,12 +1,33 @@
 package com.example.dividendproject.web;
 
 
+import com.example.dividendproject.dto.Auth;
+import com.example.dividendproject.security.TokenProvider;
+import com.example.dividendproject.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
+
+    private final MemberService memberService;
+
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Auth.Signup request) {
+        // 회원가입을 위한 컨트롤러
+        return ResponseEntity.ok().body(memberService.signUp(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody Auth.Signin request) {
+        return null;
+    }
 }

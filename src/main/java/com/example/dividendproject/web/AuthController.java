27295a8ff0cb0c73b9c10ffa62cdc8772835ca42
choidongkuen <danaver12@daum.net>
2023-03-25 14:13:27 +0900

    Feat : 로그인 컨트롤러 구현

--- src/main/java/com/example/dividendproject/web/AuthController.java
+++ src/main/java/com/example/dividendproject/web/AuthController.java
@@ -1,11 +1,14 @@
 package com.example.dividendproject.web;
 
 
+import com.example.dividendproject.domain.entity.MemberEntity;
 import com.example.dividendproject.dto.Auth;
 import com.example.dividendproject.security.TokenProvider;
 import com.example.dividendproject.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,11 +26,17 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody Auth.Signup request) {
         // 회원가입을 위한 컨트롤러
-        return ResponseEntity.ok().body(memberService.signUp(request));
+        return ResponseEntity.ok().body(this.memberService.signUp(request));
     }
 
     @PostMapping("/signin")
     public ResponseEntity<?> signin(@RequestBody Auth.Signin request) {
-        return null;
+        MemberEntity member = this.memberService.authenticate(request);
+
+        String token = this.tokenProvider.generateToken(member.getEmail(), member.getAuthority());
+
+        HttpHeaders header = new HttpHeaders();
+        header.set("Authorization",token);
+        return new ResponseEntity<>(header, HttpStatus.OK);
     }
 }

7f2816aa76d6becaa05953e39c72533c0064c9a9
choidongkuen <danaver12@daum.net>
2023-03-27 16:48:28 +0900

    Feat : SecurityConfig 구현

--- src/main/java/com/example/dividendproject/security/SecurityConfig.java
+++ src/main/java/com/example/dividendproject/security/SecurityConfig.java
@@ -1,4 +1,53 @@
 package com.example.dividendproject.security;
 
-public class SecurityConfig {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // 실제 API 관련 설정들
+
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Jwt -> Stateless
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**/signup","/**/signin").permitAll()
+                .and()
+                .addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+                // 권한 별로 권한 설정은 -> 어노테이션으로 구현 예정
+    }
+
+    @Bean // spring 2.xx 부터
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }

d91bc07e56be7ee1bc494496d00931478942d92c
choidongkuen <danaver12@daum.net>
2023-03-27 14:34:26 +0900

    Fix : MemberRepository 메소드 이름 수정
    컨트롤러로 들어가기전 필터를 거쳐 토큰 유효성 검증

--- src/main/java/com/example/dividendproject/domain/repository/MemberRepository.java
+++ src/main/java/com/example/dividendproject/domain/repository/MemberRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     Optional<MemberEntity> findByEmail(String email); // 회원 이메일을 통해 회원 엔티티를 찾는 메소드
 
-    boolean exitsByEmail(String email); // 회원가입을 할때 이미 존재하는 회원인지 확인하기 위한 메소드
+    boolean existsByEmail(String email); // 회원가입을 할때 이미 존재하는 회원인지 확인하기 위한 메소드
 }
